b761eae91b9f8380399bb00c95bc7dcba778aa1f
UrryMcA <you@example.com>
2026-05-24 23:09:35 +0700

    v0.0.1-0.0.4

--- src/texture_loader_adapter.rs
+++ src/texture_loader_adapter.rs
@@ -1,10 +1,15 @@
 use wgpu_simple_ui::common::types::TextureLoader;
+use std::path::PathBuf;
 
 pub struct FileTextureLoader;
 
 impl TextureLoader for FileTextureLoader {
     fn load_texture_rgba(&self, path: &str) -> Option<(Vec<u8>, u32, u32)> {
-        let img = image::open(path).ok()?;
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
+        let full_path = PathBuf::from(manifest_dir).join(path);
+
+
+        let img = image::open(full_path).ok()?;
         let rgba = img.to_rgba8();
         let width =  rgba.width();
         let height = rgba.height();
